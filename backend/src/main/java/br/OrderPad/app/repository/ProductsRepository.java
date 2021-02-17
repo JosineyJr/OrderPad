b57e4e1756d42983e52357017e04ae7fefaa6738
Josiney Junior <josineyjunior14@gmail.com>
2021-02-17 14:13:56 -0300

    adicionando ProductService

--- backend/src/main/java/br/OrderPad/app/repository/ProductsRepository.java
+++ backend/src/main/java/br/OrderPad/app/repository/ProductsRepository.java
@@ -4,10 +4,12 @@ import br.OrderPad.app.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Long> {
     Product findByNameStartingWith(String search);
     Product findByName(String productName);
 
-    Product findByStockGreaterThan(Integer quantity);
+    Optional<Product> findById(Integer productId);
 }
---
+++ backend/src/main/java/br/OrderPad/app/service/ProductService.java
@@ -0,0 +1,33 @@
+package br.OrderPad.app.service;
+
+import br.OrderPad.app.model.Product;
+import br.OrderPad.app.repository.ProductsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductsRepository productsRepository;
+
+    private final Logger log = LoggerFactory.getLogger(ProductService.class);
+
+    @Autowired
+    public ProductService(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    public Optional<Product> findOne(Integer productId) {
+        log.debug("REST request to get product: {}", productId);
+        return this.productsRepository.findById(productId);
+    }
+
+    public Product saveProduct(Product product) {
+        log.debug("REST request to save product: {}", product);
+        product = this.productsRepository.save(product);
+        return product;
+    }
+}
